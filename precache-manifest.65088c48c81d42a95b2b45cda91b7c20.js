1cb5c400ad8b72c96387afa87496aa180ba2cb61
NorahJC <norahjeanc@gmail.com>
2019-08-19 01:29:16 -0400

    Updates

---
+++ precache-manifest.65088c48c81d42a95b2b45cda91b7c20.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d20367647aaf318f603ed62801574741",
+    "url": "/NorahJC/robofriends.git/index.html"
+  },
+  {
+    "revision": "59d49a8330ebc98f25b7",
+    "url": "/NorahJC/robofriends.git/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "0bd867f8047e780d8c17",
+    "url": "/NorahJC/robofriends.git/static/css/main.64ae2fe6.chunk.css"
+  },
+  {
+    "revision": "59d49a8330ebc98f25b7",
+    "url": "/NorahJC/robofriends.git/static/js/2.bae3d14d.chunk.js"
+  },
+  {
+    "revision": "0bd867f8047e780d8c17",
+    "url": "/NorahJC/robofriends.git/static/js/main.c8a256e6.chunk.js"
+  },
+  {
+    "revision": "b535bc3fce11e192949a",
+    "url": "/NorahJC/robofriends.git/static/js/runtime~main.72e460ca.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/NorahJC/robofriends.git/static/media/SEGA.12a3db8b.woff"
+  }
+]);
